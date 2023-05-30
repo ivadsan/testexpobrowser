82d0c2ebf81b8f1208a8299b5c1cb172cd651eb6
Iván D. Sánchez <cosmosoft.soluciones@gmail.com>
2023-05-30 10:59:26 -0500

    instalacion basica expo-web-browser

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Button, View } from "react-native";
+import { openBrowserAsync } from "expo-web-browser";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Open Browser"
+        onPress={() =>
+          openBrowserAsync(
+            "https://beta.mercadopago.com.ar/checkout/v1/beta9/redirect?preference-id=239658604-6a98fa75-e786-48ea-9e55-3e3e018498c7&device-override=mobile"
+          )
+        }
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
